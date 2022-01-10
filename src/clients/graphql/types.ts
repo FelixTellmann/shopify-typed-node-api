8fa642a5af163dd07bf1fc3d67f7c4398893d5da
Felix <felix@tellmann.co.za>
2022-01-10 12:47:04 +0200

    Added Merge Query function for multi query requests

--- src/clients/graphql/types.ts
+++ src/clients/graphql/types.ts
@@ -2,7 +2,7 @@ import {PostRequestParams} from '../http_client/types';
 
 export type GraphqlParams<T = unknown> = Omit<PostRequestParams<T>, 'path' | 'type' | 'data'> & {
   data: string | {
-    query: string;
+    query: string | string[];
     variables?: { [K: string]: any; };
     operationName?: string;
   };
